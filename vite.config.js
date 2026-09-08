bbfe86332cde64c7c1dd3002b0463bcba4dc44a4
Evan Walker <evangibsonwalker@gmail.com>
2026-09-08 14:10:02 -0400

    updated base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/detweilers-website/',
-  
+  base: '/',
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
